7c5149658333a90e764303ff9802fdf3d56006c5
rosendolu <rosendo2018@163.com>
2020-07-21 09:35:21 +0800

    fix: insertionSort

--- algorithm/sort/insertion_sort.js
+++ algorithm/sort/insertion_sort.js
@@ -1,17 +1,22 @@
 function insertionSort(arr = []) {
-	const res = [];
-	let curVal = null;
+	let tempVal = null;
+	let tempIndex = null;
 	for (let i = 1, len = arr.length; i < len; i++) {
-		curVal = arr[i];
-		for (let j = 0; j < i; j++) {
-			if (arr[j] > curVal) {
+		tempVal = arr[i];
+		tempIndex = i;
+		for (let j = i - 1; j >= 0; j--) {
+			console.log('i,j', i, j, arr[i], arr[j]);
+			if (tempVal < arr[j]) {
 				arr[j + 1] = arr[j];
+				tempIndex = j;
 			} else {
 				break;
 			}
 		}
-		//
+		arr[tempIndex] = tempVal;
 	}
+	console.log(arr);
+	return arr;
 }
 
 export { insertionSort };
